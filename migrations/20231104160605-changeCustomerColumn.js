409e000a2dcdc5e0c79ef99e96d616536a11dbcb
Francesc Alberola <f.alberola@gmail.com>
2023-11-04 17:13:45 +0100

    validaciones customers

---
+++ migrations/20231104160605-changeCustomerColumn.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    return queryInterface.changeColumn("Customers", "email", {
+    
+    type:Sequelize.STRING,
+    
+    unique:true
+    
+    });
+    
+    },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
--- routes/customers.js
+++ routes/customers.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 router.post("/",CustomerController.create);
 router.post("/login", CustomerController.login);
-router.get("/", CustomerController.getAll);
+router.get("/",authentication, CustomerController.getAll);
 router.get("/id/:id", CustomerController.getById);
 router.get("/name/:name", CustomerController.getOneByName);
-router.put("/id/:id", CustomerController.update);
-router.delete("/id/:id", CustomerController.delete);
+router.put("/id/:id",authentication, CustomerController.update);
+router.delete("/id/:id",authentication, CustomerController.delete);
 router.delete("/logout",authentication, CustomerController.logout);
 
 module.exports = router;

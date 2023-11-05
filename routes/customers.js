f97d5610894ec26cc4ce606439c6e6eea93667c3
Francesc Alberola <f.alberola@gmail.com>
2023-11-05 12:25:22 +0100

    authentication

--- routes/customers.js
+++ routes/customers.js
@@ -4,13 +4,13 @@ const { authentication } = require ("../middleware/authentication.js")
 
 const router = express.Router();
 
-router.post("/",CustomerController.create);
-router.post("/login", CustomerController.login);
+router.post("/",authentication, CustomerController.create);
+router.post("/login",authentication, CustomerController.login);
 router.get("/", CustomerController.getAll);
 router.get("/id/:id", CustomerController.getById);
 router.get("/name/:name", CustomerController.getOneByName);
-router.put("/id/:id",authentication, CustomerController.update);
-router.delete("/id/:id",authentication, CustomerController.delete);
+router.put("/id/:id", CustomerController.update);
+router.delete("/id/:id", CustomerController.delete);
 router.delete("/logout",authentication, CustomerController.logout);
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,13 @@
 const express = require ("express");
 const ReviewController = require("../controllers/ReviewController");
+const { authentication } = require("../middleware/authentication");
 
 const router = express.Router();
 
-router.post("/create",ReviewController.create);
+router.post("/create",authentication, ReviewController.create);
 router.get("/", ReviewController.getAll);
-router.get("/id/:id",ReviewController.getById)
+router.get("/id/:id",ReviewController.getById);
+router.get("/title/:title",ReviewController.getOneByName);
+router.delete("/id/:id",ReviewController.delete)
 
 module.exports = router;
